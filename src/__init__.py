1722e8f4bf35f1d494a4006b2299f9bcbd05d67b
BGSTA9 <soheilsanatii@gmail.com>
2026-05-12 10:57:12 +0300

    Fix punctuation in module docstring for consistency

--- src/__init__.py
+++ src/__init__.py
@@ -3,9 +3,10 @@
 
 This is a simulation package for the Plane-Orthogonal-to-Skin (POS) algorithm, from 
 "Algorithmic Principles of Remote-PPG", by Wang, W., den Brinker, A. C., Stuijk, S., 
-& de Haan, G. (2017). IEEE Transactions on Biomedical Engineering
+& de Haan, G. (2017). IEEE Transactions on Biomedical Engineering.
 
 DOI: 10.1109/TBME.2016.2609282
+Published: 01-07-2017
 
 """
 
